9c67a0db2b5da2e834bcda90a9edecf7426228f2
hwangby912 <quddbs912@naver.com>
2019-11-07 11:16:17 +0900

    add DB middleware

--- Express/advanced/index.js
+++ Express/advanced/index.js
@@ -1,10 +1,29 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const user = require("./routes/user");
 const article = require("./routes/article");
 const app = express();
 
+// Customizing middleware는 반드시 next가 있어야함
+app.use((req, res, next) => {
+  mongoose
+    .connect("mongodb://localhost/express-advanced", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: true
+    })
+    .then(() => {
+      console.log("DB connected!");
+      next();
+    })
+    .catch(error => next(error)); // error를 다음 middleware로 넘김
+});
 app.use(express.json());
 app.use("/api/user", user);
 app.use("/api/article", article);
+app.use(() => {
+  mongoose.disconnect().then(() => console.log("DB Disconnected!"));
+});
 
 app.listen(3000, () => console.log("Server Running..."));
--- Express/advanced/routes/user.js
+++ Express/advanced/routes/user.js
@@ -1,13 +1,29 @@
 const express = require("express");
 const router = express.Router();
-const users = [
-  { id: 1, name: "Hwang" },
-  { id: 2, name: "Byeong" },
-  { id: 3, name: "Yoon" }
-];
+const { User, validateUser } = require("../models/user");
 
-router.get("/", (req, res) => {
+router.get("/", async (req, res) => {
+  const users = await User.find();
   res.send(users);
+});
+
+// get요청의 :id와 req.params.id의 id와 같음
+// Compass의 ObejctId와 헷갈리지 말자
+router.get("/:id", async (req, res) => {
+  const user = await User.findById(req.params.id);
+  res.send(user);
+});
+
+router.post("/", async (req, res) => {
+  if (validateUser(req.body).error) {
+    res.status(400).send("Wrong Request");
+    return;
+  }
+  const user = new User({
+    name: req.body.name
+  });
+  const result = await user.save();
+  res.status(200).send("Appropriate Request");
 });
 
 module.exports = router;
