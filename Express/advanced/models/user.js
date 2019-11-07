0efad5ce3a4e24362e062e4e66a283efc6e06401
hwangby912 <quddbs912@naver.com>
2019-11-07 10:46:06 +0900

    Express link

---
+++ Express/advanced/models/user.js
@@ -0,0 +1,29 @@
+const Joi = require("@hapi/joi");
+const mongoose = require("mongoose");
+const { Schema, model } = mongoose;
+
+const userSchema = new Schema({
+  name: { type: String, required: true, minlength: 3, maxlength: 30 },
+  articles: [{ type: mongoose.Types.ObjectId, ref: "Article" }]
+});
+
+const User = model("User", userSchema);
+
+function validateUser(user) {
+  const schema = Joi.object({
+    name: Joi.string()
+      .alphanum()
+      .min(3)
+      .max(30)
+      .required(),
+    articles: Joi.array().items(Joi.string())
+  });
+
+  // 검증 결과를 return 해줌
+  return schema.validate(user);
+}
+
+module.exports = {
+  User,
+  validateUser
+};
