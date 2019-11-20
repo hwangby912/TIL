440320088a49cd2287e3b9ab81960bd27fd35caf
hwangby912 <quddbs912@naver.com>
2019-11-20 13:53:18 +0900

    redux practice

---
+++ redux/counter/src/App.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { plus, minus } from "./store/modules/counter";
+import { changeTheme } from "./store/modules/theme";
+
+export default function App() {
+  const [color, setColor] = useState("");
+  const { number } = useSelector(state => state.counter);
+  const { theme } = useSelector(state => state.theme);
+  const dispatch = useDispatch();
+  return (
+    <div>
+      <div
+        style={{
+          width: 500,
+          height: 500,
+          backgroundColor: theme,
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center"
+        }}
+      >
+        <h1>{number}</h1>
+      </div>
+      <button onClick={() => dispatch(plus())}>Plus</button>
+      <button onClick={() => dispatch(minus())}>Minus</button>
+      <input
+        type="text"
+        value={color}
+        onChange={e => setColor(e.target.value)}
+      />
+      <button onClick={() => dispatch(changeTheme(color))}>Change Theme</button>
+    </div>
+  );
+}
